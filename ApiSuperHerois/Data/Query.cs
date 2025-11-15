6680d6e9c88fe986eea9c22d30aa56a340d735cb
gustaeps2004 <gustaeps@gmail.com>
2025-11-15 09:49:43 -0300

    adding project

---
+++ ApiSuperHerois/Data/Query.cs
@@ -0,0 +1,18 @@
+﻿using ApiSuperHerois.Entities;
+
+namespace ApiSuperHerois.Data;
+
+public class Query
+{
+    [UseProjection]
+    [UseFiltering]
+    [UseSorting]
+    public IQueryable<SuperHeroi> GetSuperherois([Service] AppDbContext context) =>
+        context.SuperHerois;
+
+	[UseProjection]
+	[UseFiltering]
+	[UseSorting]
+	public IQueryable<Filme> GetFilmes([Service] AppDbContext context) =>
+	context.Filmes;
+}
